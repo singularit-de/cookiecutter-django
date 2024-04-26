d4099bf0c54df1c9c8d53063955950e06ab90989
Janek Mangold <janek.mangold@singular-it.de>
2024-04-26 16:39:40 +0200

    Update gitlab-ci, deploy.gitlab-ci.yml, Add format=json to REST_FRAMEWORK settings

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -322,6 +322,7 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "TEST_REQUEST_DEFAULT_FORMAT": "json",
 }
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
